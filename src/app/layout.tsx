570f55d1d4af10db6c952d49011ee8398477f8bb
Vinayaka HR <vinayakahr10@gmail.com>
2025-05-31 18:08:03 +0000

    Make default theme as light and loading the page show loading indication on top and Don't load all the pages at first when it is directed to it then load it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
+import { TopProgressBar } from '@/components/layout/TopProgressBar'; // Added import
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,10 +33,11 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="dark"
+          defaultTheme="light" // Changed from dark to light
           enableSystem
           disableTransitionOnChange
         >
+          <TopProgressBar /> {/* Added TopProgressBar */}
           <Header />
           <main className="flex-grow">
             {children}
