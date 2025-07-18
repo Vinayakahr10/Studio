c25082f806dda7eb29708859582545f724b59479
Vinayaka HR <vinayakahr10@gmail.com>
2025-07-18 16:08:23 +0000

    use black color texts in nav bar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -48,7 +48,7 @@ export function Header() {
             <Button variant="link" asChild key={link.label}>
               <Link
                 href={link.href} 
-                className="text-sm font-medium text-muted-foreground transition-colors hover:text-primary px-3 py-2"
+                className="text-sm font-medium text-foreground transition-colors hover:text-primary px-3 py-2"
               >
                 <span className="flex items-center gap-1.5">
                  {link.label}
