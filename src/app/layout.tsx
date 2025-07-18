c5ac5118188c722a451567091f4f776cbbfc8b0b
Vinayaka HR <vinayakahr10@gmail.com>
2025-07-18 11:14:20 +0000

    i want add this as tumnail

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'EletronicswithVK - Your Guide to Electronics',
   description: 'Learn electronics with tutorials, projects, and articles on Arduino, Raspberry Pi, IoT, and more with EletronicswithVK.',
+  openGraph: {
+    title: 'EletronicswithVK - Your Guide to Electronics',
+    description: 'Learn electronics with tutorials, projects, and articles on Arduino, Raspberry Pi, IoT, and more with EletronicswithVK.',
+    images: [
+      {
+        url: 'https://lh3.googleusercontent.com/d/1b-f2Tm-l0H7bh8uzpY6v7XPGQgD3UBov',
+        width: 1200,
+        height: 630,
+        alt: 'An intricate circuit board with glowing pathways.',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'EletronicswithVK - Your Guide to Electronics',
+    description: 'Learn electronics with tutorials, projects, and articles on Arduino, Raspberry Pi, IoT, and more with EletronicswithVK.',
+    images: ['https://lh3.googleusercontent.com/d/1b-f2Tm-l0H7bh8uzpY6v7XPGQgD3UBov'],
+  },
 };
 
 export default function RootLayout({
