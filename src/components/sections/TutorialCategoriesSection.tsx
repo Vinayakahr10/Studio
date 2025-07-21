bb1757d196fe50cff96f6ded327e29a40632eec2
Vinayaka HR <vinayakahr10@gmail.com>
2025-07-21 16:38:47 +0000

    add 4 card  in a rows

--- src/components/sections/TutorialCategoriesSection.tsx
+++ src/components/sections/TutorialCategoriesSection.tsx
@@ -44,7 +44,7 @@ export function TutorialCategoriesSection({ categories }: TutorialCategoriesSect
           </div>
         )}
         {categoriesToDisplay.length > 0 ? (
-          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {categoriesToDisplay.map((category) => {
                 const CategoryIcon = category.Icon;
                 return(
