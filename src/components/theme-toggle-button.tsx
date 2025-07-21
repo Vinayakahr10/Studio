00764693046c6c1e015a92f478b4c6f44e58bf4b
Vinayaka HR <vinayakahr10@gmail.com>
2025-07-21 16:07:39 +0000

    remove hovering theme change turning to blue

--- src/components/theme-toggle-button.tsx
+++ src/components/theme-toggle-button.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -5,6 +6,7 @@ import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
 
 export function ThemeToggleButton() {
   const { theme, setTheme } = useTheme()
@@ -14,6 +16,7 @@ export function ThemeToggleButton() {
       variant="outline"
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+      className="hover:bg-transparent dark:hover:bg-transparent"
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
